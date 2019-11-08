4700a9439df07c6050933a62e104d1bbad8fc0e6
Nir <nir61202@gmail.com>
2019-11-09 01:03:33 +0200

    Add weird div by zero function with sse intrinsics

--- KAF-2019-DRM.cpp
+++ KAF-2019-DRM.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 #include "windows.h"
+#include <emmintrin.h>
+#include "AntiDebug.h"
 
 
-#define ExitDebug() if (IsDebuggerPresent()) {exit(1337);}
+#define CALL_FIRST 1
+#define CALL_LAST 0 
+
+LONG WINAPI
+VectoredHandlerTest(
+	struct _EXCEPTION_POINTERS* ExceptionInfo
+)
+{
+	std::cout << "exception handler\n";
+	ExceptionInfo->ContextRecord->Eip++;
+	return EXCEPTION_CONTINUE_EXECUTION;
+}
+
+
+//A weird divide by zero function with SSE instructions for "obfuscation".
+volatile int weirdDivByZero()
+{
+	__m128d xmm0 = _mm_setr_pd(0xdeadbeef8badf00d, 0x0);
+	__m128d xmm1 = _mm_setr_pd(0x0, 0x0);
+	xmm0 = _mm_div_pd(xmm0, xmm1);
+
+	int g = (int)xmm0.m128d_f64[0];
+	int c = xmm0.m128d_f64[1];
+	g += c;
+	return c / g;
+}
+
+
+//add exception handler for divide by zero for obfuscation
+//add important code in exception handler
+//add tls callback to decode resource
+
+//Idea: VM with exception codes
+
 
 int main()
 {
-	ExitDebug();
+	AddVectoredExceptionHandler(CALL_FIRST, &VectoredHandlerTest);
+	//ExitDebug();
 	std::cout << "Test!" << std::endl;
-	char _ = getchar();   
+
+	//BeingDebuggedSoftwareBreakpoint();
+	
+	//Send to an OS function to make sure that the function isn't going to be omitted.
+	SetLastError(weirdDivByZero());
+	
+
+	  
+	std::cout << "Test2!" << std::endl;
+	char _ = getchar();
+	return 0;
 }
