cbd782619f9228a8a6ba65c7e391d2ad373d4ea7
Nir <nir61202@gmail.com>
2019-12-19 21:35:41 +0200

    Add success/fail printouts

--- KAF-2019-DRM.cpp
+++ KAF-2019-DRM.cpp
@@ -17,8 +17,16 @@ int main()
 	}
 #endif
 	unsigned char opcodes[] = {static_cast<unsigned char>(Opcode::PRIV_INSTRUCTION_CMP_REG_REG), 0x02, 0x1};
-	std::cout << "Welcome to the DRM challenge. Please enter your flag to win: " << GetLastError();
-	runVmCode(opcodes, sizeof(opcodes));
+	std::cout << "Welcome to the DRM challenge. Please enter your flag to win: ";
+	
+	if (runVmCode(opcodes, sizeof(opcodes)))
+	{
+		std::cout << std::endl << "Congratulations!" << std::endl;
+	}
+	else 
+	{
+		std::cout << "This isn't the correct flag :(" << std::endl;
+	}
 
 	return 0;
 }
